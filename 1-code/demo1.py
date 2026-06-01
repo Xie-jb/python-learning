6f2e2577c2a4fb1c77380acb911dab684065d944
Boo <xjb201018@163.com>
2026-06-02 01:17:14 +0800

    完成第二个任务，筛选出包含手机号的行

--- 1-code/demo1.py
+++ 1-code/demo1.py
@@ -1,11 +1,14 @@
 #1. 读取一个 TXT 文件，统计有多少行。
 #2. 从 TXT 中筛选包含手机号的行。
 
+num = 0
 with open("customers.csv","r",encoding="utf-8") as file:
     line = file.readlines()
-    for data in line:
-        print(data)
-    col = len(line)
-    print(col)
-    
+    for data in line[1:]:
+        row = data.strip().split(",")
+        if row[3] != "":
+            print(data)
+            num += 1
+    print(num)
+
 
