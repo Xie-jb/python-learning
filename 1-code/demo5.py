431ceb8f87e73da077cd07bef92e6cdc13ce6bef
Boo <xjb201018@163.com>
2026-06-02 19:32:32 +0800

    增添几个.c文件，用于完成批量改名任务

---
+++ 1-code/demo5.py
@@ -0,0 +1,21 @@
+
+## 练习任务
+
+# 1. 列出某个文件夹下所有文件。
+# 2. 按后缀分类文件，例如图片、文档、表格。
+# 3. 批量给图片加编号。
+# 4. 生成文件清单 TXT 或 Excel。
+
+import os
+from pathlib import Path
+foldPath = Path("./2-file")
+index = 0
+for file in foldPath.iterdir():
+    if file.suffix == ".c":
+        index += 1
+        new_name = f"test_{index:03d}{file.suffix}"
+        new_path = foldPath / new_name
+        file.rename(new_path)
+    #print(file)
+
+
